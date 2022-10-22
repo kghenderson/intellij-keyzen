e70e9e60a4303e88e845996e1f10fabf2d38ebfe
GitHub Action <action@github.com>
2022-10-22 17:03:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/kghenderson/intellijkeyzen/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kghenderson/intellijkeyzen/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kghenderson.intellijkeyzen.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kghenderson.intellijkeyzen.MyBundle
 
 class MyApplicationService {
 
